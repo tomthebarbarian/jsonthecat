2926a577ba69f86c999d559a6e5002047e024e33
Tom Zhang <tom.zhang989@gmail.com>
2021-09-23 15:47:53 -0400

    version of catFetch for submit

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,26 @@
-const request = require(request);
+const request = require('request');
+// https://api.thecatapi.com/v1/images/search?breed_ids={breed-id}
+const usableargs = process.argv;
+
+let breedNM = usableargs[2];
+
+
+const nameSearch = `htttps://api.thecatapi.com/v1/breeds/search?q=${breedNM}`;
+
+// const address = catAPI;
+
+const catrequest = request(nameSearch, (error, response, body) => {
+  if (error !== null) {
+    console.log('url not found');
+    return;
+  }
+
+  if (response) {
+    // console.log(response);
+  }
+  let catobj = JSON.parse(body);
+
+  // console.log(typeof catobj);
+  console.log(catobj[0].description);
+  return;
+});
